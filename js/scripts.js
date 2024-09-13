f40ad1fa2e020cd70063f0c3db6f10a526e6d427
Adrian Fusco Arnejo <afuscoar@redhat.com>
2024-09-14 01:14:30 +0200

    Improve styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const projectsList = document.getElementById('projects-list');
+  const projectsList = document.getElementById('projects-list');
+  const pinnedRepos = [
+      { name: 'openinfra2024-dashboard-as-a-code', description: 'This repository contains supplementary materials for our presentation at the OpenInfra Day Germany 2024 in Berlin.', url: 'https://github.com/adrianfusco/openinfra2024-dashboard-as-a-code' },
+      { name: 'openinfra2024-software-factory-playground', description: 'This repository contains supplementary materials for our presentation at the OpenInfra Summit Asia 2024 in Korea.', url: 'https://github.com/adrianfusco/openinfra2024-software-factory-playground' },
+      { name: 'CI Framework', description: 'CI Framework - used for CI, QE and Devs to run jobs in a converged way', url: 'https://github.com/openstack-k8s-operators/ci-framework/' },
+      { name: 'Syncron', description: 'Usage Patterns all in one cli based application written in Golang.', url: 'https://github.com/RedHatCRE/syncron' },
+      { name: 'EksS3ClusterApp', description: 'The project provide a solution for creating S3 buckets and adding specific IAM policies using Terraform modules and automate the creation of a AWS EKS cluster where we will have different resources to access into S3 bucket through a Kubernetes Ingress resource.', url: 'https://github.com/adrianfusco/EksS3ClusterApp' },
+      { name: 'Cibyl', description: 'Cibyl is a command-line interface and REST API for querying CI/CD environments and systems.', url: 'https://github.com/RedHatCRE/cibyl' },
+  ];
 
-    // We can't fetch from GitHub API pinned repos and we can use GraphQL in the APIv4 but we need Auth for that so... let's just avoid it :)
-    const pinnedRepos = [
-        { name: 'openinfra2024-dashboard-as-a-code', description: 'This repository contains supplementary materials for our presentation at the OpenInfra Day Germany 2024 in Berlin.', url: 'https://github.com/adrianfusco/openinfra2024-dashboard-as-a-code' },
-        { name: 'openinfra2024-software-factory-playground', description: 'This repository contains supplementary materials for our presentation at the OpenInfra Summit Asia 2024 in Korea.', url: 'https://github.com/adrianfusco/openinfra2024-software-factory-playground' },
-        { name: 'CI Framework', description: 'CI Framework - used for CI, QE and Devs to run jobs in a converged way', url: 'https://github.com/openstack-k8s-operators/ci-framework/' },
-        { name: 'Syncron', description: 'Usage Patterns all in one cli based application written in Golang.', url: 'https://github.com/RedHatCRE/syncron' },
-        { name: 'EksS3ClusterApp', description: 'The project provide a solution for creating S3 buckets and adding specific IAM policies using Terraform modules and automate the creation of a AWS EKS cluster where we will have different resources to access into S3 bucket through a Kubernetes Ingress resource.', url: 'https://github.com/adrianfusco/EksS3ClusterApp' },
-        { name: 'Cibyl', description: 'Cibyl is a command-line interface and REST API for querying CI/CD environments and systems.', url: 'https://github.com/RedHatCRE/cibyl' },
-    ];
-
-    pinnedRepos.forEach(repo => {
-        const projectItem = document.createElement('div');
-        projectItem.classList.add('p-6', 'bg-gray-100', 'dark:bg-gray-800', 'shadow-md', 'rounded-lg');
-        projectItem.innerHTML = `
-        <h4 class="text-xl font-bold text-gray-900 dark:text-white mb-2">${repo.name}</h4>
-        <p class="text-gray-700 dark:text-gray-300">${repo.description || 'No description available'}</p>
-        <a href="${repo.url}" class="text-blue-600 dark:text-blue-400 underline hover:text-blue-500 dark:hover:text-blue-300" target="_blank">View on GitHub</a>
-      `;
-        projectsList.appendChild(projectItem);
-    });
+  pinnedRepos.forEach(repo => {
+      const projectItem = document.createElement('div');
+      projectItem.classList.add('p-6', 'bg-gray-100', 'dark:bg-gray-800', 'shadow-md', 'rounded-lg', 'mb-6');
+      projectItem.innerHTML = `
+      <h4 class="text-xl font-bold text-gray-900 dark:text-white mb-2">${repo.name}</h4>
+      <a href="${repo.url}" class="text-blue-600 dark:text-blue-400 underline hover:text-blue-500 dark:hover:text-blue-300 mb-4 block" target="_blank">View on GitHub</a>
+      <p class="text-gray-700 dark:text-gray-300">${repo.description || 'No description available'}</p>
+    `;
+      projectsList.appendChild(projectItem);
+  });
 });
+
 
 const translations = {
     en: {
